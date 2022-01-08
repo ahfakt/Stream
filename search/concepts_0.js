f902f65c09c30ddb42410d67f0cc6e705d9b2c09
ahfakt <a.farukakts@outlook.com>
2022-01-08 17:05:29 +0300

    Changed concepts

--- search/concepts_0.js
+++ search/concepts_0.js
@@ -1,8 +1,7 @@
 var searchData=
 [
   ['constructible_0',['Constructible',['../db/d8f/conceptStream_1_1Constructible.html',1,'Stream']]],
-  ['constructibledeserializable_1',['ConstructibleDeserializable',['../d6/d37/conceptStream_1_1ConstructibleDeserializable.html',1,'Stream']]],
-  ['deserializable_2',['Deserializable',['../d3/d34/conceptStream_1_1Deserializable.html',1,'Stream']]],
-  ['extractable_3',['Extractable',['../d4/d91/conceptStream_1_1Extractable.html',1,'Stream']]],
-  ['trivialdeserializable_4',['TrivialDeserializable',['../df/d1b/conceptStream_1_1TrivialDeserializable.html',1,'Stream']]]
+  ['deserializable_1',['Deserializable',['../d3/d34/conceptStream_1_1Deserializable.html',1,'Stream']]],
+  ['extractable_2',['Extractable',['../d4/d91/conceptStream_1_1Extractable.html',1,'Stream']]],
+  ['serializable_3',['Serializable',['../de/d51/conceptStream_1_1Serializable.html',1,'Stream']]]
 ];
