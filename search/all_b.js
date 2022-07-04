007b24790e859d151a8a07e46f535178bf1391dd
ahfakt <a.farukakts@outlook.com>
2022-07-04 20:47:30 +0300

    ...

--- search/all_b.js
+++ search/all_b.js
@@ -1,12 +1,8 @@
 var searchData=
 [
-  ['constructible_0',['Constructible',['../db/d8f/conceptStream_1_1Constructible.html',1,'Stream']]],
-  ['deserializable_1',['Deserializable',['../d3/d34/conceptStream_1_1Deserializable.html',1,'Stream']]],
-  ['extractable_2',['Extractable',['../d4/d91/conceptStream_1_1Extractable.html',1,'Stream']]],
-  ['serializable_3',['Serializable',['../de/d51/conceptStream_1_1Serializable.html',1,'Stream']]],
-  ['setbuffersize_4',['setBufferSize',['../../IO/d4/d80/classIO_1_1Pipe.html#a68891217b93786ad8ebaf5bc6a2bb872',1,'IO::Pipe']]],
-  ['sockaddr_5',['sockaddr',['../../IO/d5/de1/classsockaddr.html',1,'']]],
-  ['socket_6',['Socket',['../../IO/d1/d7c/classIO_1_1Socket.html',1,'IO::Socket'],['../../IO/d1/d7c/classIO_1_1Socket.html#af8fc52979afe41d9f7ccd7f1e68d17b5',1,'IO::Socket::Socket()'],['../../IO/d1/d7c/classIO_1_1Socket.html#a61fac24b003b64ab8933878eb0d1ed85',1,'IO::Socket::Socket(INET const &amp;address, int backlog)'],['../../IO/d1/d7c/classIO_1_1Socket.html#a02bbdffd70026bab8addf9dba163e1fd',1,'IO::Socket::Socket(INET const &amp;address)'],['../d1/db1/classStream_1_1Socket.html',1,'Stream::Socket']]],
-  ['socketinput_7',['SocketInput',['../dc/d10/classStream_1_1SocketInput.html',1,'Stream']]],
-  ['socketoutput_8',['SocketOutput',['../d5/d52/classStream_1_1SocketOutput.html',1,'Stream']]]
+  ['setbuffersize_0',['setBufferSize',['../../IO/d4/d80/classIO_1_1Pipe.html#a68891217b93786ad8ebaf5bc6a2bb872',1,'IO::Pipe']]],
+  ['sockaddr_1',['sockaddr',['../../IO/d5/de1/classsockaddr.html',1,'']]],
+  ['socket_2',['Socket',['../../IO/d1/d7c/classIO_1_1Socket.html',1,'IO::Socket'],['../../IO/d1/d7c/classIO_1_1Socket.html#af8fc52979afe41d9f7ccd7f1e68d17b5',1,'IO::Socket::Socket()'],['../../IO/d1/d7c/classIO_1_1Socket.html#a61fac24b003b64ab8933878eb0d1ed85',1,'IO::Socket::Socket(INET const &amp;address, int backlog)'],['../../IO/d1/d7c/classIO_1_1Socket.html#a02bbdffd70026bab8addf9dba163e1fd',1,'IO::Socket::Socket(INET const &amp;address)'],['../d1/db1/classStream_1_1Socket.html',1,'Stream::Socket']]],
+  ['socketinput_3',['SocketInput',['../dc/d10/classStream_1_1SocketInput.html',1,'Stream']]],
+  ['socketoutput_4',['SocketOutput',['../d5/d52/classStream_1_1SocketOutput.html',1,'Stream']]]
 ];
