ead08b6b2929ba912dfc3fa05fa1f7268e9e4146
ahfakt <a.farukakts@outlook.com>
2022-08-04 01:50:43 +0300

    .

--- search/concepts_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['constructible_0',['Constructible',['../db/d8f/conceptStream_1_1Constructible.html',1,'Stream']]],
-  ['deserializable_1',['Deserializable',['../d3/d34/conceptStream_1_1Deserializable.html',1,'Stream']]],
-  ['extractable_2',['Extractable',['../d4/d91/conceptStream_1_1Extractable.html',1,'Stream']]],
-  ['serializable_3',['Serializable',['../de/d51/conceptStream_1_1Serializable.html',1,'Stream']]]
-];
