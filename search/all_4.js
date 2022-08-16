e1977fb088764cdc6564eec7a00eab95dc1844d0
ahfakt <a.farukakts@outlook.com>
2022-08-17 01:40:30 +0300

    .

--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['file_0',['File',['../df/d21/classStream_1_1File.html#a012a2b61d8e7ba3a46511b7bf8cfd8d1',1,'Stream::File::File()'],['../df/d21/classStream_1_1File.html',1,'Stream::File']]]
+  ['file_0',['File',['../df/d21/classStream_1_1File.html#a012a2b61d8e7ba3a46511b7bf8cfd8d1',1,'Stream::File::File()'],['../df/d21/classStream_1_1File.html',1,'Stream::File']]],
+  ['flush_1',['flush',['../df/d21/classStream_1_1File.html#a8a1102c808e8c9d8bbcb8af849393677',1,'Stream::File']]]
 ];
