aa51bde0de2bb9eb7cbc099df84126b5e1ec2abb
ahfakt <a.farukakts@outlook.com>
2023-07-02 19:23:16 +0300

    .

--- search/all_b.js
+++ search/all_b.js
@@ -1,6 +1,10 @@
 var searchData=
 [
-  ['setbuffersize_0',['setBufferSize',['../d3/de2/classStream_1_1Pipe.html#a843d48a003c21f2503b8412032af57e6',1,'Stream::Pipe']]],
-  ['sockaddr_1',['sockaddr',['../d5/de1/classsockaddr.html',1,'']]],
-  ['socket_2',['Socket',['../d1/db1/classStream_1_1Socket.html#a673fc3261f9cfe2ccc37068428a07aa2',1,'Stream::Socket::Socket()'],['../d1/db1/classStream_1_1Socket.html#a91288528be3eedbf16cb23e8872ce084',1,'Stream::Socket::Socket(Address const &amp;address, int backlog)'],['../d1/db1/classStream_1_1Socket.html#a56d3aa4a9a8c637f5aa1439f5554e27b',1,'Stream::Socket::Socket(Address const &amp;address)'],['../d1/db1/classStream_1_1Socket.html',1,'Stream::Socket']]]
+  ['char_0',['Char',['../d1/d93/conceptStream_1_1Char.html',1,'Stream']]],
+  ['integer_1',['Integer',['../d4/d6d/conceptStream_1_1Integer.html',1,'Stream']]],
+  ['setbuffersize_2',['setBufferSize',['../d3/de2/classStream_1_1Pipe.html#a843d48a003c21f2503b8412032af57e6',1,'Stream::Pipe']]],
+  ['signedint_3',['SignedInt',['../d3/db1/conceptStream_1_1SignedInt.html',1,'Stream']]],
+  ['sockaddr_4',['sockaddr',['../d5/de1/classsockaddr.html',1,'']]],
+  ['socket_5',['Socket',['../d1/db1/classStream_1_1Socket.html#a673fc3261f9cfe2ccc37068428a07aa2',1,'Stream::Socket::Socket()'],['../d1/db1/classStream_1_1Socket.html#a91288528be3eedbf16cb23e8872ce084',1,'Stream::Socket::Socket(Address const &amp;address, int backlog)'],['../d1/db1/classStream_1_1Socket.html#a56d3aa4a9a8c637f5aa1439f5554e27b',1,'Stream::Socket::Socket(Address const &amp;address)'],['../d1/db1/classStream_1_1Socket.html',1,'Stream::Socket']]],
+  ['unsignedint_6',['UnsignedInt',['../d9/dce/conceptStream_1_1UnsignedInt.html',1,'Stream']]]
 ];
---
+++ search/concepts_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['char_0',['Char',['../d1/d93/conceptStream_1_1Char.html',1,'Stream']]],
+  ['integer_1',['Integer',['../d4/d6d/conceptStream_1_1Integer.html',1,'Stream']]],
+  ['signedint_2',['SignedInt',['../d3/db1/conceptStream_1_1SignedInt.html',1,'Stream']]],
+  ['unsignedint_3',['UnsignedInt',['../d9/dce/conceptStream_1_1UnsignedInt.html',1,'Stream']]]
+];
